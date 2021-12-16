b6ba74894334545be870f860385580b0f0888651
Andre Scheiermann <scheiermann.andre@gmail.com>
2021-12-16 16:14:47 +0100

    dev: created project

---
+++ android/app/src/main/kotlin/com/example/mylib/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mylib
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ commitlint.config.js
@@ -0,0 +1,14 @@
+const Configuration = {
+
+  extends: ['@commitlint/config-conventional'],
+
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {
+        "type-enum": [2, "always", ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test", "dev"]],
+  },
+};
+
+module.exports = Configuration;
+
